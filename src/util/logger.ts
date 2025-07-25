0bac556c0f3182fd687878df508e6b97b3cbf431
Sandhya110 <starsandhya369@gmail.com>
2025-07-25 20:24:07 +0530

    feat: Colorize terminal

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,92 +1,102 @@
-import {createLogger, format, transports} from 'winston'
+import { createLogger, format, transports } from 'winston'
 import { ConsoleTransportInstance, FileTransportInstance } from 'winston/lib/winston/transports'
 import util from 'util'
 import config from '../config/config'
 import { EApplicationEnvironment } from '../constant/application'
 import path from 'path'
-import * as SourceMapSupport from  'source-map-support'
+import * as SourceMapSupport from 'source-map-support'
+import { red, blue, yellow, magenta, green } from 'colorette'
 
 //linking trace support
 SourceMapSupport.install()
 
+ 
+const colorizeLevel = (level: string) => {
+    switch (level) {
+        case 'ERROR':
+            return red(level)
+        case 'INFO':
+            return blue(level)
+        case 'YARN':
+            return yellow(level)
+        default:
+            return level
+    }
+}
 
 const consoleLogFormat = format.printf((info) => {
+    const { level, message, timestamp, meta = {} } = info
 
-  const {level,message,timestamp,meta = {} } = info
+    const customLevel = colorizeLevel(level.toUpperCase())
 
-  const customLevel = level.toUpperCase()
+    const customTimestamp = green(timestamp as string)
 
-  const customTimestamp = timestamp
+    const customMessage = message
 
-  const customMessage = message
+    const customMeta = util.inspect(meta, {
+        showHidden: false,
+        depth: null,
+        colors: true
+    })
 
-  const customMeta = util.inspect(meta, {
-    showHidden: false,
-    depth: null
-  })
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    const customLog = `${customLevel} [${customTimestamp}] ${customMessage}\n${magenta('META')} ${customMeta}\n`
 
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-  const customLog = `${customLevel} [${customTimestamp}] ${customMessage}\n${'META'} ${customMeta}\n`
-
-  return customLog
-
+    return customLog
 })
 
-const consoleTransport = ():Array<ConsoleTransportInstance> => {
-  if(config.ENV === EApplicationEnvironment.DEVELOPMENT){
-    return [
-    new transports.Console({
-      level: 'info',
-      format: format.combine(format.timestamp(), consoleLogFormat)
-    })
-  ]
-  }
+const consoleTransport = (): Array<ConsoleTransportInstance> => {
+    if (config.ENV === EApplicationEnvironment.DEVELOPMENT) {
+        return [
+            new transports.Console({
+                level: 'info',
+                format: format.combine(format.timestamp(), consoleLogFormat)
+            })
+        ]
+    }
 
-  return []
-
+    return []
 }
 
 const fileLogFormat = format.printf((info) => {
+    const { level, message, timestamp, meta = {} } = info
 
-  const {level,message,timestamp,meta = {} } = info
+    const logMeta: Record<string, unknown> = {}
 
-  const logMeta:Record<string, unknown> = {}
-
-  for(const [key, value] of Object.entries(meta as Record<string, unknown>)){
-    if(value instanceof Error){
-      logMeta[key] = {
-        name: value.name,
-        message: value.message,
-        trace: value.stack || ''
-      }
-    }else{
-      logMeta[key] = value
+    for (const [key, value] of Object.entries(meta as Record<string, unknown>)) {
+        if (value instanceof Error) {
+            logMeta[key] = {
+                name: value.name,
+                message: value.message,
+                trace: value.stack || ''
+            }
+        } else {
+            logMeta[key] = value
+        }
     }
-  }
 
-  const logData = {
-    level: level.toUpperCase(),
-    message,
-    timestamp,
-    meta: logMeta
-  }
-     return JSON.stringify(logData, null, 4)  
-
+    const logData = {
+        level: level.toUpperCase(),
+        message,
+        timestamp,
+        meta: logMeta
+    }
+    return JSON.stringify(logData, null, 4)
 })
 
-const fileTransport = ():Array<FileTransportInstance> => {
-  return [
-    new transports.File({
-      filename: path.join(__dirname, '../', '../', 'logs', `${config.ENV}.log`),
-      level: 'info',
-      format: format.combine(format.timestamp(), fileLogFormat)
-    })
-  ]
+const fileTransport = (): Array<FileTransportInstance> => {
+    return [
+        new transports.File({
+            filename: path.join(__dirname, '../', '../', 'logs', `${config.ENV}.log`),
+            level: 'info',
+            format: format.combine(format.timestamp(), fileLogFormat)
+        })
+    ]
 }
 
 export default createLogger({
-  defaultMeta: {
-    meta: {}
-  },
-  transports: [...fileTransport(), ...consoleTransport()]
+    defaultMeta: {
+        meta: {}
+    },
+    transports: [...fileTransport(), ...consoleTransport()]
 })
