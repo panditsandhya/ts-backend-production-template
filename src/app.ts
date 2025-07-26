2ac717261a3d46b791daadad0698da5a87438e0f
Sandhya110 <starsandhya369@gmail.com>
2025-07-26 19:56:31 +0530

    feat: Changes in helmet js

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,7 @@ import helmet from 'helmet';
 const app: Application = express();
 
 //middleware
-app.use(helmet({
-  
-}))
+app.use(helmet())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../', 'public')))
 
